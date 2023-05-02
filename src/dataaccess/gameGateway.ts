1cc25ddd8806e267f5b185a1b4b4769f8cda1188
Masakuni <xxbambooxx@icloud.com>
2023-05-03 01:22:07 +0900

    add:data access layer

---
+++ src/dataaccess/gameGateway.ts
@@ -0,0 +1,28 @@
+import mysql from 'mysql2/promise'
+import { GameRecord } from "./gameRecord";
+
+export class GameGateway {
+  async findLatest(conn: mysql.Connection): Promise<GameRecord | undefined> {
+    const gameSelectResult = await conn.execute<mysql.RowDataPacket[]>(
+      'select id, started_at from games order by id desc limit 1'
+    )
+    const record = gameSelectResult[0][0]
+
+    if (!record) {
+      return undefined
+    }
+
+    return new GameRecord(record['id'], record['started_at'])
+  }
+
+  async insert(conn: mysql.Connection, started_at: Date): Promise<GameRecord> {
+    const gameInsertResult = await conn.execute<mysql.ResultSetHeader>(
+      'insert into games (started_at) values (?)',
+      [started_at]
+    )
+    const gameId = gameInsertResult[0].insertId
+
+    return new GameRecord(gameId, started_at)
+  }
+
+}
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,10 @@ import express from 'express'
 import morgan from 'morgan'
 import 'express-async-errors'
 import mysql from 'mysql2/promise'
+import { GameGateway } from './dataaccess/gameGateway'
+import { TurnGateway } from './dataaccess/turnGateway'
+import { MoveGateway } from './dataaccess/moveGateway'
+import { SquareGateway } from './dataaccess/squareGateway'
 
 const EMPTY = 0
 const DARK = 1
@@ -24,6 +28,12 @@ const app = express()
 
 app.use(morgan('dev'))
 app.use(express.static('static', { extensions: ['html'] }))
+app.use(express.json())
+
+const gameGateway = new GameGateway()
+const turnGateway = new TurnGateway()
+const moveGateway = new MoveGateway()
+const squareGateway = new SquareGateway()
 
 app.get('/api/hello', async (req, res) => {
   res.json({
@@ -37,40 +47,107 @@ app.get('/api/error', async (req, res) => {
 
 app.post('/api/games', async (req, res) => {
   const now = new Date()
-  const conn = await mysql.createConnection({
-    host: 'localhost',
-    database: 'reversi',
-    user: 'reversi',
-    password: 'password'
-  })
+  const conn = await connectMySQL()
 
-  try{
+  try {
     await conn.beginTransaction()
 
-    const gameInsertResult = await conn.execute<mysql.ResultSetHeader>('insert into games (started_at) values (?)', [now])
-    const gameId = gameInsertResult[0].insertId
+    const gameRecord = await gameGateway.insert(conn, now)
+    const turnRecord = await turnGateway.insert(conn, gameRecord.id, 0, DARK, now)
 
-    const turnInsertResult = await conn.execute<mysql.ResultSetHeader>('insert into turns (game_id, turn_count, next_disc, end_at) values (?, ?, ?, ?)', [gameId, 0, DARK, now])
-    const turnId = turnInsertResult[0].insertId
-
-    const squareCount = INITIAL_BOARD.map((line) => line.length).reduce((v1, v2) => v1 + v2, 0)
-    const squareInsertSql = 'insert into squares (turn_id, x, y, disc) values ' + Array.from(Array(squareCount)).map(() => '(?, ?, ?, ?)').join(',')
-    const squareInsertValues: any[] = []
-    INITIAL_BOARD.forEach((line, y) => {
-      line.forEach((disc, x) => {
-        squareInsertValues.push(turnId)
-        squareInsertValues.push(x)
-        squareInsertValues.push(y)
-        squareInsertValues.push(disc)
-      })
-    })
-
-    await conn.execute(squareInsertSql, squareInsertValues)
+    await squareGateway.insertAll(conn, turnRecord.id, INITIAL_BOARD)
 
     await conn.commit()
   } finally {
     await conn.end()
   }
+
+  res.status(201).end()
+})
+
+app.get('/api/games/latest/turns/:turnCount', async (req, res) => {
+  const turnCount = parseInt(req.params.turnCount)
+
+  const conn = await connectMySQL()
+
+  try {
+
+    const gameRecord = await gameGateway.findLatest(conn)
+    if (!gameRecord) {
+      throw new Error('Latest game not found')
+    }
+    const turnRecord = await turnGateway.findForGameIdAndTurnCount(conn, gameRecord.id, turnCount)
+    if (!turnRecord) {
+      throw new Error('Supecified turn not found')
+    }
+
+    const squareRecords = await squareGateway.findForTurnId(conn, turnRecord.id)
+    const board = Array.from(Array(8)).map(() => Array.from(Array(8)))
+    squareRecords.forEach((s) => {
+      board[s.y][s.x] = s.disc
+    })
+    const responseBody = {
+      turnCount,
+      board,
+      nextDisc: turnRecord.nextDisc,
+      // TODO 決着がついている場合、game_resultテーブルから取得する
+      winnerDisc: null
+    }
+    res.json(responseBody)
+  } finally {
+    await conn.end()
+  }
+
+})
+
+app.post('/api/games/latest/turns/', async (req, res) => {
+  const turnCount = parseInt(req.body.turnCount)
+  const disc = parseInt(req.body.move.disc)
+  const x = parseInt(req.body.move.x)
+  const y = parseInt(req.body.move.y)
+
+  const conn = await connectMySQL()
+
+  try {
+    // 1つ前のターンを取得する
+    const gameRecord = await gameGateway.findLatest(conn)
+    if (!gameRecord) {
+      throw new Error('Latest game not found')
+    }
+    const previousTurnCount = turnCount - 1
+    const previousTurnRecord = await turnGateway.findForGameIdAndTurnCount(conn, gameRecord.id, previousTurnCount)
+    if (!previousTurnRecord) {
+      throw new Error('Supecified turn not found')
+    }
+
+    const squareRecords = await squareGateway.findForTurnId(conn, previousTurnRecord.id)
+    const board = Array.from(Array(8)).map(() => Array.from(Array(8)))
+    squareRecords.forEach((s) => {
+      board[s.y][s.x] = s.disc
+    })
+    // 盤面に置けるかチェックする
+
+    // 石を置く
+    board[y][x] = disc
+    console.log(board)
+
+    // ひっくり返す
+
+    // ターンを保存する
+    const nextDisc = disc === DARK ? LIGHT : DARK
+    const now = new Date()
+    const turnRecord = await turnGateway.insert(conn, gameRecord.id, turnCount, nextDisc, now)
+
+    await squareGateway.insertAll(conn, turnRecord.id, board)
+
+    await moveGateway.insert(conn, turnRecord.id, disc, x, y)
+
+    await conn.commit()
+  } finally {
+    await conn.end()
+  }
+
+
 
   res.status(201).end()
 })
@@ -87,4 +164,13 @@ function errorHandler(err: any, _req: express.Request, res: express.Response, _n
   res.status(500).send({
     message: 'Unexpected error occured'
   })
-} 
+}
+
+async function connectMySQL() {
+  return mysql.createConnection({
+    host: 'localhost',
+    database: 'reversi',
+    user: 'reversi',
+    password: 'password'
+  })
+}
