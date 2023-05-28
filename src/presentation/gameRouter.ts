9b99be98c784151b896b3ba1cf3dae2781ff2e07
Masakuni <xxbambooxx@icloud.com>
2023-05-29 00:29:40 +0900

    feat:add result page

--- src/presentation/gameRouter.ts
+++ src/presentation/gameRouter.ts
@@ -2,16 +2,53 @@ import express from 'express'
 import { StartNewGameUseCase } from '../application/useCase/startNewGameUseCase'
 import { GameMySQLRepository } from '../infrastructure/repository/game/gameMySQLRepository'
 import { TurnMySQLRepository } from '../infrastructure/repository/turn/turnMySQLRepository'
+import { FindLastGamesMySQLQueryService } from '../infrastructure/query/findLastGamesMySQLQueryService'
+import { FindLastGamesUseCase } from '../application/useCase/findLastGamesUseCase'
 
 export const gameRouter = express.Router()
 
+const startNewGameUseCase = new StartNewGameUseCase(
+  new GameMySQLRepository(),
+  new TurnMySQLRepository()
+)
+
+const fincLastGamesUseCase = new FindLastGamesUseCase(
+  new FindLastGamesMySQLQueryService
+)
+
+interface GetGamesResponseBody {
+  games: {
+    id: number
+    darkMoveCount: number
+    lightMoveCount: number
+    winnerDisc: number,
+    startedAt: Date
+    endAt: Date
+  }[]
+}
+
+gameRouter.get('/api/games', async (req, res: express.Response<GetGamesResponseBody>) => {
+  const output = await fincLastGamesUseCase.run()
+
+  const responseBodyGames = output.map((g) => {
+    return {
+      id: g.gameId,
+      darkMoveCount: g.darkMoveCount,
+      lightMoveCount: g.lightMoveCount,
+      winnerDisc: g.winnerDisc,
+      startedAt: g.startedAt,
+      endAt: g.endAt
+    }
+  })
+
+  const responseBody = {
+    games: responseBodyGames
+  }
+
+  res.json(responseBody)
+})
 
 gameRouter.post('/api/games', async (req, res) => {
-
-  const startNewGameUseCase = new StartNewGameUseCase(
-    new GameMySQLRepository(),
-    new TurnMySQLRepository()
-  )
   await startNewGameUseCase.run()
 
   res.status(201).end()
